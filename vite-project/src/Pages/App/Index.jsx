be397007b9f151fd94e7d0a37c568e70f5b33399
dacerbo <david.acerbo@mercadolibre.com>
2023-06-10 13:23:32 -0300

    update

--- vite-project/src/Pages/App/Index.jsx
+++ vite-project/src/Pages/App/Index.jsx
@@ -1,3 +1,6 @@
+// HOOKS
+import {useRoutes, BrowserRouter} from 'react-router-dom';
+
 import "./App.css";
 import Home from '../Home';
 import MyAccount from '../MyAccount';
@@ -6,15 +9,26 @@ import MyOrders from '../MyOrders';
 import SignIn from '../SignIn';
 import NotFoundignIn from '../NotFound';
 
+
+const AppRoutes = () => {
+  const routes = useRoutes([
+    {path: '/', element: <Home />},
+    {path: '/my-account', element: <MyAccount />},
+    {path: '/my-order', element: <MyOrder />},
+    {path: '/my-orders', element: <MyOrders />},
+    {path: '/sign-in', element: <SignIn />},
+    {path: '*', element: <NotFoundignIn />},
+  ])
+
+  return routes
+}
+
 const App = () => {
   return (
     <div className="bg-red-900">
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <SignIn />
-      <NotFoundignIn />
+      <BrowserRouter>
+        <AppRoutes />      
+      </BrowserRouter>
     </div>
   )
 }
